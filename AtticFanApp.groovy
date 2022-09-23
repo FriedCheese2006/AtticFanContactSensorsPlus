305a0ae39f7e49304d7574cad6fcc63d426fb281
FriedCheese2006 <49689423+FriedCheese2006@users.noreply.github.com>
2022-09-23 13:19:28 -0400

    Update AtticFanApp.groovy

--- AtticFanApp.groovy
+++ AtticFanApp.groovy
@@ -13,8 +13,6 @@
  * LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
  *
  * v1.0		RLE		Creation
- * v1.1     RLE     Added list attribute to show triggered devices
- * v1.2     RLE     Added threshold input and associated logic
  */
  
 definition(
