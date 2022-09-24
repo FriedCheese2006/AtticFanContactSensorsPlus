19ad3ed151726122e58739d341359b42b7b86755
FriedCheese2006 <49689423+FriedCheese2006@users.noreply.github.com>
2022-09-24 16:03:20 -0400

    Update WindowTracker_Parent.groovy

--- WindowTracker_Parent.groovy
+++ WindowTracker_Parent.groovy
@@ -49,7 +49,7 @@ def mainPage() {
 			}
 			section("Child Apps") {
 				app(name: "windowTrackerApp+", appName: "Window Tracker Child", namespace: "rle.sg+", title: "Add a new Window Tracker Instance", multiple: true)
-				app(name: "overwatch", appName: "Window Summary", namespace: "rle.sg+", title: "Add a new Overwatch Instance", multiple: true)
+				app(name: "overwatch", appName: "Window Tracker Aggregate", namespace: "rle.sg+", title: "Add a new Aggregate Instance", multiple: true)
 			}
 			section("General") {
        			label title: "Enter a name for this parent app (optional)", required: false
